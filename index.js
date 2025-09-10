76cf49705f273d4457fd630ec820329235136435
angeldominguezlopez3-blip <angeldominguezlopez3@gmail.com>
2025-09-09 18:19:28 -0600

    proyecto_visual_studio

--- index.js
+++ index.js
@@ -1,11 +1,58 @@
-const http = require('http');
+const express = require("express");
+const admin = require("firebase-admin");
+const cors = require("cors");
+const app = express();
+app.use(cors());
 
-const app = http.createServer((request,response)=>{
-    response.writeHead(200, {'Content-Type': 'application/json'})
-    response.end('servidor node iniciado')
-})
 
-const PORT = 3000
-app.listen(PORT)
-console.log('Servidor levantado en el puerto 3000')
+// Inicializar Firebase
+app.use(express.json());
 
+const serviceAccount = require("./firebase-key.json");
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
+
+// Rutas
+const db = admin.firestore();
+
+app.get("/clientes/EqHZF0KnXdF4ibkB7EJF", (req, res) => { //Ruta GET
+  res.send("Servidor corriendo Firebase");});
+
+  // Crear documento usuario
+app.post("/clientes/EqHZF0KnXdF4ibkB7EJF", async (req, res) => { //Ruta POST
+  try {
+    const { nombre, email, telefono } = req.body;    
+    // Agregar documento a la colección "usuarios"   
+    const docRef = await db.collection("clientes").add({ email, nombre, telefono }); 
+    res.json({ id: docRef.id, message: "Cliente agregado" });  
+    } 
+    catch (error) {
+    res.status(500).json({ error: error.message });  
+    }
+});
+
+// Obtener datos de los documentos
+app.get("/clientes/EqHZF0KnXdF4ibkB7EJF", async (req, res) => {
+  try {
+    const items = await db.collection("clientes").get();
+
+    const usuarios = items.docs.map(doc => { // Mapear documentos a un array de objetos
+      const data = doc.data();
+      return {
+        id: doc.id,
+        nombre: data.nombre,
+        email: data.email,
+        telefono: data.telefono
+      };
+    });
+
+    res.json(usuarios); // Enviar array de usuarios como respuesta en JSON
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+// Conexión al servidor
+const PORT = 3000;
+app.listen(PORT, () => console.log(`Servidor corriendo en http://localhost:${PORT}`));
